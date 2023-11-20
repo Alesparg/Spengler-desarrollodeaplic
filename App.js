545cc9306272d93ad2f10f429d91c93f6d5ab74e
Alejandro Spengler <spengleralejandro@gmail.com>
2023-11-20 10:26:46 -0300

    prpyecto base

--- App.js
+++ App.js
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from 'react-native';
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hola Coder !!!</Text>
+      <Text>Hola Coder !!! Como andan</Text>
       <StatusBar style="auto" />
     </View>
   );
